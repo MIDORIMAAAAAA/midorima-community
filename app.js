0b9aef172b562a5f2f80af20c12c2f46c06b1a4e
MIDORIMAAAAAA <81515096+MIDORIMAAAAAA@users.noreply.github.com>
2021-03-29 18:58:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+    const Discord = require('discord.js');
+    require('dotenv').config();
+    const client = new Discord.Client();
+
+    client.on('ready', () => {
+        console.log('Bot is ready');
+      });
+
+      client.login(process.env.BOT_TOKEN)
+
+      client.on('message', (msg) => {
+        if (msg.content === 'Salut') msg.reply('Salut !');
+      });
+      
+}
